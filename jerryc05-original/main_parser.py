429cecc7552fae4cdb6ae36ca7181a711aa3e1a8
Jerry Chen <jerryc05@qq.com>
2019-05-30 20:29:59 -0400

    change format and README.md
    Signed-off-by: Jerry Chen <jerryc05@qq.com>

--- jerryc05-original/main_parser.py
+++ jerryc05-original/main_parser.py
@@ -1,64 +1,71 @@
-def colored_text(text: str, fore='RESET', back='RESET',
-                 style='RESET_ALL') -> None:
-    """Print text in colored format.
 
-    :param text: Text to format.
-    :param fore: Foreground color to format, default='RESET'.
-    :param back: Background color to format, default='RESET'.
-    :param style: Style to format, default='RESET_ALL'.
 
-    Available colors:
-        RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, BLACK, RESET.
+def colored_text(
+				text: str,
+				fore='RESET',
+				back='RESET',
+				style='RESET_ALL'
+) -> None:
+	r"""Print text in colored format.
 
-    Available styles:
-        DIM, NORMAL, BRIGHT, RESET_ALL.
-    """
+	:param text: Text to format.
+	:param fore: Foreground color to format, default='RESET'.
+	:param back: Background color to format, default='RESET'.
+	:param style: Style to format, default='RESET_ALL'.
 
-    prt = print
+	Available colors:
+			RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, BLACK, RESET.
 
-    fore  =  fore.upper() # yapf: disable
-    back  =  back.upper() # yapf: disable
-    style = style.upper()
+	Available styles:
+			DIM, NORMAL, BRIGHT, RESET_ALL.
+	"""
 
-    import colorama
-    colorama.init(autoreset=True)
+	prt = print
 
-    c_fore = colorama.Fore
-    fores = {
-        'RED'       : c_fore.RED,
-        'GREEN'     : c_fore.GREEN,
-        'YELLOW'    : c_fore.YELLOW,
-        'BLUE'      : c_fore.BLUE,
-        'MAGENTA'   : c_fore.MAGENTA,
-        'CYAN'      : c_fore.CYAN,
-        'WHITE'     : c_fore.WHITE,
-        'BLACK'     : c_fore.BLACK,
-        'RESET'     : c_fore.RESET,
-    } # yapf: disable
-    if back == 'RESET' and style == 'RESET_ALL':
-        prt(f'{fores.get(fore.upper(), "")}{text}')
-        return
+	fore = fore.upper()
+	back = back.upper()
+	style = style.upper()
 
-    c_back = colorama.Back
-    backs = {
-        'RED'       : c_back.RED,
-        'GREEN'     : c_back.GREEN,
-        'YELLOW'    : c_back.YELLOW,
-        'BLUE'      : c_back.BLUE,
-        'MAGENTA'   : c_back.MAGENTA,
-        'CYAN'      : c_back.CYAN,
-        'WHITE'     : c_back.WHITE,
-        'BLACK'     : c_back.BLACK,
-        'RESET'     : c_back.RESET,
-    } # yapf: disable
-    c_style = colorama.Style
-    styles = {
-        'DIM'       : c_style.DIM,
-        'NORMAL'    : c_style.NORMAL,
-        'BRIGHT'    : c_style.BRIGHT,
-        'RESET_ALL' : c_style.RESET_ALL,
-    } # yapf: disable
-    prt(f'{styles.get(style.upper(), "")}'
-        f'{ fores.get( fore.upper(), "")}'
-        f'{ backs.get( back.upper(), "")}'
-        f'{text}')
+	import colorama
+	colorama.init(autoreset=True)
+	c_fore = colorama.Fore
+	fores = {
+		'RED'    : c_fore.RED,
+		'GREEN'  : c_fore.GREEN,
+		'YELLOW' : c_fore.YELLOW,
+		'BLUE'   : c_fore.BLUE,
+		'MAGENTA': c_fore.MAGENTA,
+		'CYAN'   : c_fore.CYAN,
+		'WHITE'  : c_fore.WHITE,
+		'BLACK'  : c_fore.BLACK,
+		'RESET'  : c_fore.RESET,
+	}
+	if back == 'RESET' and style == 'RESET_ALL':
+		prt(f'{fores.get(fore.upper(), "")}{text}')
+		return
+
+	c_back = colorama.Back
+	backs = {
+		'RED'    : c_back.RED,
+		'GREEN'  : c_back.GREEN,
+		'YELLOW' : c_back.YELLOW,
+		'BLUE'   : c_back.BLUE,
+		'MAGENTA': c_back.MAGENTA,
+		'CYAN'   : c_back.CYAN,
+		'WHITE'  : c_back.WHITE,
+		'BLACK'  : c_back.BLACK,
+		'RESET'  : c_back.RESET,
+	}
+	c_style = colorama.Style
+	styles = {
+		'DIM'      : c_style.DIM,
+		'NORMAL'   : c_style.NORMAL,
+		'BRIGHT'   : c_style.BRIGHT,
+		'RESET_ALL': c_style.RESET_ALL,
+	}
+	prt(
+		f'{styles.get(style.upper(), "")}'
+		f'{fores.get(fore.upper(), "")}'
+		f'{backs.get(back.upper(), "")}'
+		f'{text}'
+	)
